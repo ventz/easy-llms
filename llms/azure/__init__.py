d4b2d42d6ec9baf9f2031a6ca7ea39f672384465
Ventz Petkov <ventz.petkov@cerbo.io>
2024-07-10 12:41:11 -0400

    Fix for azure_gpt_35_turbo_16k - thanks Ben - and update to README for Claude v3.5

--- llms/azure/__init__.py
+++ llms/azure/__init__.py
@@ -1,6 +1,6 @@
 # azure/__init__.py
 from .gpt35turbo import AzureGPT35Turbo as azure_gpt_35_turbo
-from .gpt35turbo16k import AzureGPT35Turbo16k as azure_gpt35_turbo_16k
+from .gpt35turbo16k import AzureGPT35Turbo16k as azure_gpt_35_turbo_16k
 from .gpt4turbo import AzureGPT4Turbo as azure_gpt_4_turbo
 from .gpt4o import AzureGPT4o as azure_gpt_4o
 from .gpt4 import AzureGPT4 as azure_gpt_4
